2f6698b3d386ecac964be3d1bc0a4c8b81795d70
R.I.Pienaar <rip@devco.net>
2010-08-06 16:10:13 +0100

    Issue 112 - Use several YAML files as a fact source
    A list of yaml files can now be specified in standard unix :
    seperated list style.  These YAML files will be merged with
    later ones overriding earlier ones should the same fact
    be in a few of the files.
    
    Signed-off-by: R.I.Pienaar <rip@devco.net>

--- plugins/mcollective/facts/yaml.rb
+++ plugins/mcollective/facts/yaml.rb
@@ -3,14 +3,23 @@ module MCollective
         require 'yaml'
 
         # A factsource that reads a hash of facts from a YAML file
+        #
+        # Multiple files can be specified seperated with a : in the
+        # config file, they will be merged with later files overriding
+        # earlier ones in the list.
         class Yaml<Base
             def get_facts
                 config = Config.instance
 
+                fact_files = config.pluginconf["yaml"].split(":")
                 facts = {}
 
-                YAML.load_file(config.pluginconf["yaml"]).each_pair do |k, v|
-                    facts[k] = v.to_s
+                fact_files.each do |file|
+                    if File.exist?(file)
+                        facts.merge!(YAML.load_file(file))
+                    else
+                        Log.instance.error("Can't find YAML file to load: #{file}")
+                    end
                 end
 
                 facts
