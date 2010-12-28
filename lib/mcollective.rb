0ef0da57010bb0e9a4af6a8b97c36fa299b723f9
R.I.Pienaar <rip@devco.net>
2010-12-28 16:25:19 +0000

    5700 - Security plugin exceptions should be better handled
    - Add a new exception type and handle it in the client loop
    - Adjust the security plugins to raise the new exception
    
    Machines that do not pass security checks will now be handled as
    if they did not respond at all.
    
    They will show up in the stat[:noresponsefrom] stats hash and be
    available just like all other non responding hosts.
    
    A log at level WARN will be logged for them.

--- lib/mcollective.rb
+++ lib/mcollective.rb
@@ -27,6 +27,7 @@ module MCollective
     class InvalidRPCData<RPCError;end
     class UnknownRPCError<RPCError;end
     class NotTargettedAtUs<RuntimeError;end
+    class SecurityValidationFailed<RuntimeError;end
     class DDLValidationError<RuntimeError;end
 
     autoload :Config, "mcollective/config"
--- website/changelog.md
+++ website/changelog.md
@@ -11,6 +11,7 @@ title: Changelog
 
 |Date|Description|Ticket|
 |----|-----------|------|
+|2010/12/29|Treat machines that fail security validation like ones that did not respond|5700|
 |2010/12/25|Increase the rpcutil timeout to allow for slow facts|5679|
 |*2010/12/13*|*Release 1.0.0*|5453|
 
