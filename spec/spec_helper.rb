f57f6b1dac90d3dd0e32c450704d9fb65cb5c540
R.I.Pienaar <rip@devco.net>
2011-04-04 17:58:58 +0100

    maint: specify a specific version of mocha to use

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,9 +2,12 @@ dir = File.expand_path(File.dirname(__FILE__))
 $LOAD_PATH.unshift("#{dir}/")
 $LOAD_PATH.unshift("#{dir}/../lib")
 
-require 'mcollective'
 require 'rubygems'
+
+gem 'mocha', :version => "=0.9.10"
+
 require 'rspec'
+require 'mcollective'
 require 'rspec/mocks'
 require 'mocha'
 require 'ostruct'
