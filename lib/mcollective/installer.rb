924643e9cb129c274440da773856aaf384799f6c
eranb <barakviz@gmail.com>
2011-10-31 18:52:37 +0200

    added gem support

--- lib/mcollective/installer.rb
+++ lib/mcollective/installer.rb
@@ -10,8 +10,7 @@ module MCollective
     DEFAULT_BIN_PATH     = "/usr/sbin"
     INSTALLATION_TYPES = [:client,:server,:common,:full]
     
-    def initialize(type = nil)
-      @type    = type || :full
+    def initialize
       @options = {
         :ruby_path    => DEFAULT_RUBY_PATH,
         :config_path  => DEFAULT_CFG_PATH,
@@ -20,18 +19,31 @@ module MCollective
         :force        => false
       }
       parse!
+      @type = ARGV.shift || :full
     end
     
     def install!
-      abort "Unknown installation type #{@type.inspect}" unless INSTALLATION_TYPES.include? @type
+      abort "Unknown installation type #{@type.inspect}" unless INSTALLATION_TYPES.include? @type.to_sym
       send @type
     end
     
-    private  
+    private
     def server
       FileUtils.mkdir_p @options[:config_path] + "/ssl" unless File.directory? @options[:config_path] + "/ssl"
-      cp "mcollectived.rb",     "/usr/sbin/mcollectived"
-      cp "mcollective.init-rh", "/etc/init.d/" if File.directory? "/etc/init.d" # TODO: on suse different init script
+      daemon_path = "/usr/sbin/mcollectived"
+      unless File.exists?(daemon_path) && !@options[:force]
+        mcollectived = File.new(daemon_path,'w')
+        mcollectived.puts File.read("#{base}/mcollectived.rb").gsub("#!/usr/bin/env ruby","#!#{@options[:ruby_path]}/ruby")
+        mcollectived.close
+        FileUtils.chmod 0777,daemon_path
+      end
+      
+      # TODO: on suse different init script
+      if File.directory? "/etc/init.d"
+        cp "mcollective.init-rh", "/etc/init.d/mcollective"  
+        FileUtils.chmod 0777,"/etc/init.d/mcollective"
+      end
+      
       cp "etc/facts.yaml.dist", "#{@options[:config_path]}/facts.yaml"
       cp "etc/server.cfg.dist", "#{@options[:config_path]}/server.cfg"
       cp "etc/rpc-help.erb",    "#{@options[:config_path]}/rpc-help.erb"
@@ -42,11 +54,9 @@ module MCollective
       cp "etc/client.cfg.dist","#{@options[:config_path]}/client.cfg"
       Dir.chdir base
       Dir.glob("{mc-*,mco}").each do |f| 
-        dest = "#{@options[:bin_path]}/#{f}"
-        cp f,dest
-        FileUtils.chmod 0777,dest
+        cp f,"#{@options[:bin_path]}/#{f}"
+        FileUtils.chmod 0777,"#{@options[:bin_path]}/#{f}"
       end
-      
     end
     
     def common
@@ -55,15 +65,17 @@ module MCollective
     end
     
     define_method(:full) {[:server,:client,:common].each {|m| send m}}
-    
+        
     def parse!
       OptionParser.new do |opts|
-        opts.banner = "Usage: mcollective_install"
+        opts.banner = "Usage: mcollective_install [options] #{INSTALLATION_TYPES.join ','}"
         opts.separator ""
         opts.separator "Options:"
-        opts.on("-r", "--ruby_path PATH", "default is #{@options[:ruby]}")  { |path| @options[:ruby_path]   = path }
-        opts.on("-c", "--config_path PATH", "default is #{@options[:cfg]}") { |path| @options[:config_path] = path }
-        opts.on("-f", "--force", "replace files, default is #{@options[:force]}") {  @options[:force]       = true }
+        opts.on("-r", "--ruby_path PATH",    "default is #{@options[:ruby_path]}")    { |path| @options[:ruby_path]   = path }
+        opts.on("-c", "--config_path PATH",  "default is #{@options[:config_path]}")  { |path| @options[:config_path] = path }
+        opts.on("-p", "--plugins_path PATH", "default is #{@options[:plugins_path]}") { |path| @options[:plugins_path] = path }
+        opts.on("-b", "--bin_path PATH",     "default is #{@options[:bin_path]}")     { |path| @options[:bin_path] = path }
+        opts.on("-f", "--force",             "replace files, default is #{@options[:force]}") {  @options[:force]       = true }
       end.parse!
     end
     
