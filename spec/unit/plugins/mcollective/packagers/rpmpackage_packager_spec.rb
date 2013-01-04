59d3dffec5487f8c474a6697e067baaeb3178575
Pieter Loubser <pieter.loubser@puppetlabs.com>
2013-01-04 14:50:39 +0000

    16909 - Tests spam console with sh: rpm: not found on Debian
    The previous commit for this ticket dit not correctly remove the console
    spam when running tests on debian.
    
    This commit removes the 'rpm not found' spam when running tests on
    debian.

--- spec/unit/plugins/mcollective/packagers/rpmpackage_packager_spec.rb
+++ spec/unit/plugins/mcollective/packagers/rpmpackage_packager_spec.rb
@@ -22,8 +22,8 @@ module MCollective
         @plugin.stubs(:iteration).returns("1")
         @plugin.stubs(:metadata).returns({:name => "test", :version => "1"})
         @plugin.stubs(:mcname).returns("mcollective")
-        @plugin.stubs(:rpmdir)
-        @plugin.stubs(:srpmdir)
+        RpmpackagePackager.any_instance.stubs(:rpmdir).returns('rpmdir')
+        RpmpackagePackager.any_instance.stubs(:srpmdir).returns('srpmdir')
       end
 
       after :all do
