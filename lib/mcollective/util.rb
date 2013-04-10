5a83fb9708fa062c08f9424f9528795f141fafd3
Matthaus Owens <matthaus@puppetlabs.com>
2013-04-09 23:09:11 -0700

    20388 - Mcollective should have sane defaults for configfiles on windows
    This commit adds sane defaults for the server.cfg and client.cfg files.
    The other configuration files can then be specified in those two files.
    As these merely add defaults, any commandline flags for configfile
    location will override the default.

--- lib/mcollective/util.rb
+++ lib/mcollective/util.rb
@@ -135,6 +135,12 @@ module MCollective
        "compound" => []}
     end
 
+    # Returns the PuppetLabs mcollective path for windows
+    def self.windows_prefix
+      require 'win32/dir'
+      prefix = File.join(Dir::COMMON_APPDATA, "PuppetLabs", "mcollective")
+    end
+
     # Picks a config file defaults to ~/.mcollective
     # else /etc/mcollective/client.cfg
     def self.config_file_for_user
@@ -145,10 +151,18 @@ module MCollective
         config = File.expand_path("~/.mcollective")
 
         unless File.readable?(config) && File.file?(config)
-          config = "/etc/mcollective/client.cfg"
+          if self.windows?
+            config = File.join(self.windows_prefix, "etc", "client.cfg")
+          else
+            config = "/etc/mcollective/client.cfg"
+          end
         end
       rescue Exception => e
-        config = "/etc/mcollective/client.cfg"
+        if self.windows?
+          config = File.join(self.windows_prefix, "etc", "client.cfg")
+        else
+          config = "/etc/mcollective/client.cfg"
+        end
       end
 
       return config
