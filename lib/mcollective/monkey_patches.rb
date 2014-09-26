d684b853cd00520918ea0aff0872ae47bb1b9ada
Richard Clamp <richardc@unixbeard.net>
2014-09-25 23:58:18 -0700

    MCO-136 enforce Style/SpaceInsideRangeLiteral

--- lib/mcollective/monkey_patches.rb
+++ lib/mcollective/monkey_patches.rb
@@ -2,7 +2,7 @@
 # 1.8.5 and 1.8.6
 class String
   def start_with?(str)
-    return self[0 .. (str.length-1)] == str
+    return self[0..(str.length-1)] == str
   end unless method_defined?("start_with?")
 end
 
