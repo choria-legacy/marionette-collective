a431e2fbe241bf56302eada0bd79d8d08f09991c
John Duarte <john.duarte@puppetlabs.com>
2015-06-19 17:11:04 -0700

    (MAINT) Add mco_master role to acceptance
    This commit adds an `mco_master` role to the beaker host files,
    as well as using this role in the tests and pre-suit files to
    determine where to deploy the mco-broker infrastructure.
    
    Since the `mco_master` role can be applied to any node in the
    hosts lists, the `server.cfg` file has been added to the
    `060_Install-activemq.rb` pre-suite step as a parameterized
    HEREDOC string.

--- acceptance/setup/aio/pre-suite/070_Install_puppet-agent-plugin.rb
+++ acceptance/setup/aio/pre-suite/070_Install_puppet-agent-plugin.rb
@@ -1,26 +1,27 @@
 test_name 'install puppet-agent plugin' do
-  if agent.platform =~ /windows/ then
-   mco_libdir = 'C:/ProgramData/PuppetLabs/mcollective/etc/plugins/mcollective'
-  else
-   mco_libdir = '/opt/puppetlabs/mcollective/plugins/mcollective'
-   git_pkg = 'git'
-   if agent.platform =~ /ubuntu-10/
-     git_pkg = 'git-core'
-   end
-   install_package(agent, git_pkg)
-  end
-  on agent, "mkdir -p #{mco_libdir}"
-  on agent, "git clone https://github.com/puppetlabs/mcollective-puppet-agent.git"
-  on agent, "cd mcollective-puppet-agent && for i in agent aggregate application data util validator ; do cp -a $i #{mco_libdir} ; done"
+  hosts.each do |h|
+    if h.platform =~ /windows/ then
+     mco_libdir = 'C:/ProgramData/PuppetLabs/mcollective/etc/plugins/mcollective'
+    else
+     mco_libdir = '/opt/puppetlabs/mcollective/plugins/mcollective'
+     git_pkg = 'git'
+     if h.platform =~ /ubuntu-10/
+       git_pkg = 'git-core'
+     end
+     install_package(h, git_pkg)
+    end
+    on h, "mkdir -p #{mco_libdir}"
+    on h, "git clone https://github.com/puppetlabs/mcollective-puppet-agent.git"
+    on h, "cd mcollective-puppet-agent && for i in agent aggregate application data util validator ; do cp -a $i #{mco_libdir} ; done"
 
-  unless agent.platform =~/windows/ then
-    on agent, 'service mcollective restart'
-  else
-    on agent, puppet('resource service mcollective ensure=stopped')
-    on agent, puppet('resource service mcollective ensure=running')
+    unless h.platform =~/windows/ then
+      on h, 'service mcollective restart'
+    else
+      on h, puppet('resource service mcollective ensure=stopped')
+      on h, puppet('resource service mcollective ensure=running')
+    end
   end
-
-  unless port_open_within?(agent, 61613, 300 )
+  unless port_open_within?(mco_master, 61613, 300 )
     raise Beaker::DSL::FailTest, 'Timed out trying to access ActiveMQ'
   end
 end
--- acceptance/tests/mco_ping.rb
+++ acceptance/tests/mco_ping.rb
@@ -1,7 +1,7 @@
 test_name "mco ping" do
   step "run ping"
-    if agent.platform =~ /windows/ then
-      if agent[:ruby_arch] == 'x86' then
+    if mco_master.platform =~ /windows/ then
+      if mco_master[:ruby_arch] == 'x86' then
         mco_bin = 'cmd.exe /c C:/Program\ Files\ \(x86\)/Puppet\ Labs/Puppet/bin/mco.bat'
       else
         mco_bin = 'cmd.exe /c C:/Program\ Files/Puppet\ Labs/Puppet/bin/mco.bat'
@@ -9,8 +9,8 @@ test_name "mco ping" do
     else
      mco_bin = '/opt/puppetlabs/bin/mco'
     end
-    on(agent, "#{mco_bin} ping") do
+    on(mco_master, "#{mco_bin} ping") do
       assert_equal(0, exit_code)
-      assert_match(/^1\s+replies/, stdout)
+      assert_match(/^#{hosts.size}\s+replies/, stdout)
     end
 end
