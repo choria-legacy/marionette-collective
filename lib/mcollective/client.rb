choria-legacy/marionette-collective

commit 928ccac63ae2820bc47679264c12e4adf3ba08c6

(MCO-777) Wait for all expected responses to rpc, report surprises
Modifies mco to wait for responses from all discovered hosts, rather
than stopping when the expected number of hosts have responded. This
ensures that, if a node doesn't respond to discovery but responds to the
rpc, we don't lose responses from all discovered nodes.

Also update stats to print all responses that came from nodes that did
not respond to discovery. This simplifies debugging issues where nodes
respond unexpectedly, allowing us to identify the nodes behaving
unexpectedly.

lib/mcollective/client.rb
@@ -159,10 +159,10 @@ module MCollective
     #
     # It returns a hash of times and timeouts for discovery and total run is taken from the options
     # hash which in turn is generally built using MCollective::Optionparser
-    def req(body, agent=nil, options=false, waitfor=0, &block)
+    def req(body, agent=nil, options=false, waitfor=[], &block)
       if body.is_a?(Message)
         agent = body.agent
-        waitfor = body.discovered_hosts.size || 0
+        waitfor = body.discovered_hosts || []
         @options = body.options
       end
 
@@ -174,7 +174,6 @@ module MCollective
       stat = {:starttime => Time.now.to_f, :discoverytime => 0, :blocktime => 0, :totaltime => 0}
       STDOUT.sync = true
       hosts_responded = 0
-
 
       begin
         if threaded
@@ -238,16 +237,39 @@ module MCollective
     def start_receiver(requestid, waitfor, timeout, &block)
       Log.debug("Starting response receiver with timeout of #{timeout}")
       hosts_responded = 0
+
+      if (waitfor.is_a?(Array))
+        unfinished = Hash.new(0)
+        waitfor.each {|w| unfinished[w] += 1}
+      else
+        unfinished = []
+      end
+
       begin
         Timeout.timeout(timeout) do
-          begin
+          loop do
             resp = receive(requestid)
             yield resp.payload
             hosts_responded += 1
-          end while (waitfor == 0 || hosts_responded < waitfor)
+
+            if (waitfor.is_a?(Array))
+              sender = resp.payload[:senderid]
+              if unfinished[sender] <= 1
+                unfinished.delete(sender)
+              else
+                unfinished[sender] -= 1
+              end
+
+              break if !waitfor.empty? && unfinished.empty?
+            else
+              break unless waitfor == 0 || hosts_responded < waitfor
+            end
+          end
         end
       rescue Timeout::Error => e
-        if (waitfor > hosts_responded)
+        if (waitfor.is_a?(Array) && !unfinished.empty?)
+          Log.warn("Could not receive all responses. Did not receive responses from #{unfinished.keys.join(', ')}")
+        elsif (waitfor > hosts_responded)
           Log.warn("Could not receive all responses. Expected : #{waitfor}. Received : #{hosts_responded}")
         end
       end

spec/unit/mcollective/client_spec.rb
@@ -205,24 +205,24 @@ module MCollective
 
       it "should thread the publisher and receiver if configured" do
         client.instance_variable_get(:@options)[:threaded] = true
-        client.expects(:threaded_req).with(request, 2, 0, 1)
+        client.expects(:threaded_req).with(request, 2, 0, ['rspec'])
         message.options[:threaded] = true
         client.req(message)
       end
 
       it "should not thread the publisher and receiver if configured" do
         client.instance_variable_set(:@threaded, false)
-        client.expects(:unthreaded_req).with(request, 2, 0, 1)
+        client.expects(:unthreaded_req).with(request, 2, 0, ['rspec'])
         client.req(message)
       end
 
       it "uses the publish_timeout from options when passed as an option" do
-        client.expects(:unthreaded_req).with(request, 5, 0, 1)
+        client.expects(:unthreaded_req).with(request, 5, 0, ['rspec'])
         client.req(message, nil, message.options.merge(:publish_timeout => 5))
       end
 
       it "uses the publish_timeout from config when passed as a config value" do
-        client.expects(:unthreaded_req).with(request, 10, 0, 1)
+        client.expects(:unthreaded_req).with(request, 10, 0, ['rspec'])
         client.instance_variable_get(:@config).expects(:publish_timeout).returns(10)
         client.req(message)
       end
@@ -278,50 +278,114 @@ module MCollective
     end
 
     describe "#start_receiver" do
-      it "should go into a receive loop and receive until it reaches waitfor" do
-        results = []
-        Timeout.stubs(:timeout).yields
-        message = mock
-        client.stubs(:receive).with("erfs123").returns(message)
-        message.stubs(:payload).returns("msg1", "msg2", "msg3")
-        client.start_receiver("erfs123", 3, 5) do |msg|
-          results << msg
+      describe "waitfor is a number" do
+        it "should go into a receive loop and receive until it reaches waitfor" do
+          results = []
+          Timeout.stubs(:timeout).yields
+          message = mock
+          client.stubs(:receive).with("erfs123").returns(message)
+          message.stubs(:payload).returns("msg1", "msg2", "msg3")
+          client.start_receiver("erfs123", 3, 5) do |msg|
+            results << msg
+          end
+          results.should == ["msg1", "msg2", "msg3"]
         end
-        results.should == ["msg1", "msg2", "msg3"]
+
+        it "should log a warning if a timeout occurs" do
+          results = []
+          Timeout.stubs(:timeout).yields
+          message = mock
+          client.stubs(:receive).with("erfs123").returns(message)
+          message.stubs(:payload).returns("msg1", "msg2", "timeout")
+          Log.expects(:warn).with("Could not receive all responses. Expected : 3. Received : 2")
+          responded = client.start_receiver("erfs123", 3, 5) do |msg|
+            if msg == "timeout"
+              raise Timeout::Error
+            end
+            results << msg
+          end
+          results.should == ["msg1", "msg2"]
+          responded.should == 2
+        end
+
+        it "should not log a warning if a the response count is larger or equal to the expected number of responses" do
+          results = []
+          Timeout.stubs(:timeout).yields
+          message = mock
+          client.stubs(:receive).with("erfs123").returns(message)
+          message.stubs(:payload).returns("msg1", "msg2", "timeout")
+          Log.expects(:warn).never
+          responded = client.start_receiver("erfs123", 2, 5) do |msg|
+            if msg == "timeout"
+              raise Timeout::Error
+            end
+            results << msg
+          end
+          results.should == ["msg1", "msg2"]
+          responded.should == 2
+        end
       end
 
-      it "should log a warning if a timeout occurs" do
-        results = []
-        Timeout.stubs(:timeout).yields
-        message = mock
-        client.stubs(:receive).with("erfs123").returns(message)
-        message.stubs(:payload).returns("msg1", "msg2", "timeout")
-        Log.expects(:warn).with("Could not receive all responses. Expected : 3. Received : 2")
-        responded = client.start_receiver("erfs123", 3, 5) do |msg|
-          if msg == "timeout"
-            raise Timeout::Error
+      describe "waitfor is an array" do
+        it "should go into a receive loop and receive until it matches waitfor" do
+          senders = ["sender1", "sender2", "sender3", "sender4"]
+          expected = senders.map {|s| Message.new({:callerid => "caller", :senderid => s}, nil, :type => :reply)}
+          results = []
+          Timeout.stubs(:timeout).yields
+          client.stubs(:receive).with("erfs123").returns(*expected)
+          client.start_receiver("erfs123", senders[0,3], 5) do |msg|
+            results << msg
           end
-          results << msg
+          results.should == expected[0,3].map {|m| m.payload}
         end
-        results.should == ["msg1", "msg2"]
-        responded.should == 2
-      end
 
-      it "should not log a warning if a the response count is larger or equal to the expected number of responses" do
-        results = []
-        Timeout.stubs(:timeout).yields
-        message = mock
-        client.stubs(:receive).with("erfs123").returns(message)
-        message.stubs(:payload).returns("msg1", "msg2", "timeout")
-        Log.expects(:warn).never
-        responded = client.start_receiver("erfs123", 2, 5) do |msg|
-          if msg == "timeout"
-            raise Timeout::Error
+        it "receive until it gets all expected responses" do
+          senders = ["sender1", "sender2", "sender3", "sender4"]
+          expected = senders.map {|s| Message.new({:callerid => "caller", :senderid => s}, nil, :type => :reply)}
+          results = []
+          Timeout.stubs(:timeout).yields
+          client.stubs(:receive).with("erfs123").returns(*expected)
+          client.start_receiver("erfs123", senders[1,3], 5) do |msg|
+            results << msg
           end
-          results << msg
+          results.should == expected.map {|m| m.payload}
         end
-        results.should == ["msg1", "msg2"]
-        responded.should == 2
+
+        it "should log a warning if a timeout occurs" do
+          senders = ["sender1", "sender2", "sender3"]
+          messages = ["msg1", "msg2", "timeout"]
+          expected = senders.zip(messages).map {|s, m| Message.new({:callerid => "caller", :senderid => s, :body => m}, nil, :type => :reply)}
+          results = []
+          Timeout.stubs(:timeout).yields
+          client.stubs(:receive).with("erfs123").returns(*expected)
+          Log.expects(:warn).with("Could not receive all responses. Did not receive responses from sender3")
+          responded = client.start_receiver("erfs123", senders, 5) do |msg|
+            if msg[:body] == "timeout"
+              raise Timeout::Error
+            end
+            results << msg
+          end
+          results.should == expected[0,2].map {|m| m.payload}
+          responded.should == 2
+        end
+
+        it "should not log a warning if a the response count is larger or equal to the expected number of responses" do
+          senders = ["sender1", "sender2", "sender3"]
+          messages = ["msg1", "msg2", "timeout"]
+          expected = senders.zip(messages).map {|s, m| Message.new({:callerid => "caller", :senderid => s, :body => m}, nil, :type => :reply)}
+          results = []
+          Timeout.stubs(:timeout).yields
+          client.stubs(:receive).with("erfs123").returns(*expected)
+          Log.expects(:warn).never
+          responded = client.start_receiver("erfs123", senders[0,2], 5) do |msg|
+            if msg[:body] == "timeout"
+              raise Timeout::Error
+            end
+            results << msg
+          end
+          results.should == expected[0,2].map {|m| m.payload}
+          responded.should == 2
+        end
       end
     end
 

lib/mcollective/rpc/client.rb
@@ -869,6 +869,7 @@ module MCollective
             end
 
             @stats.noresponsefrom.concat @client.stats[:noresponsefrom]
+            @stats.unexpectedresponsefrom.concat @client.stats[:unexpectedresponsefrom]
             @stats.responses += @client.stats[:responses]
             @stats.blocktime += @client.stats[:blocktime] + sleep_time
             @stats.totaltime += @client.stats[:totaltime]

spec/unit/mcollective/rpc/client_spec.rb
@@ -774,7 +774,7 @@ module MCollective
           msg.expects(:create_reqid).returns("823a3419a0975c3facbde121f72ab61f")
           msg.expects(:requestid=).with("823a3419a0975c3facbde121f72ab61f").times(10)
 
-          stats = {:noresponsefrom => [], :responses => 0, :blocktime => 0, :totaltime => 0, :discoverytime => 0, :requestid => "823a3419a0975c3facbde121f72ab61f"}
+          stats = {:noresponsefrom => [], :unexpectedresponsefrom => [], :responses => 0, :blocktime => 0, :totaltime => 0, :discoverytime => 0, :requestid => "823a3419a0975c3facbde121f72ab61f"}
 
           Message.expects(:new).with('req', nil, {:type => :direct_request, :agent => 'foo', :filter => nil, :options => {}, :collective => 'mcollective'}).returns(msg).times(10)
           client.expects(:new_request).returns("req")
@@ -802,7 +802,7 @@ module MCollective
           msg.expects(:create_reqid).returns("823a3419a0975c3facbde121f72ab61f")
           msg.expects(:requestid=).with("823a3419a0975c3facbde121f72ab61f").times(10)
 
-          stats = {:noresponsefrom => [], :responses => 0, :blocktime => 0, :totaltime => 0, :discoverytime => 0, :requestid => "823a3419a0975c3facbde121f72ab61f"}
+          stats = {:noresponsefrom => [], :unexpectedresponsefrom => [], :responses => 0, :blocktime => 0, :totaltime => 0, :discoverytime => 0, :requestid => "823a3419a0975c3facbde121f72ab61f"}
 
           Progress.expects(:new).never
 

lib/mcollective/rpc/stats.rb
@@ -2,9 +2,9 @@ module MCollective
   module RPC
     # Class to wrap all the stats and to keep track of some timings
     class Stats
-      attr_accessor :noresponsefrom, :starttime, :discoverytime, :blocktime, :responses, :totaltime
-      attr_accessor :discovered, :discovered_nodes, :okcount, :failcount, :noresponsefrom, :responsesfrom
-      attr_accessor :requestid, :aggregate_summary, :ddl, :aggregate_failures
+      attr_accessor :noresponsefrom, :unexpectedresponsefrom, :starttime, :discoverytime, :blocktime, :responses
+      attr_accessor :totaltime, :discovered, :discovered_nodes, :okcount, :failcount, :noresponsefrom
+      attr_accessor :responsesfrom, :requestid, :aggregate_summary, :ddl, :aggregate_failures
 
       def initialize
         reset
@@ -13,6 +13,7 @@ module MCollective
       # Resets stats, if discovery time is set we keep it as it was
       def reset
         @noresponsefrom = []
+        @unexpectedresponsefrom = []
         @responsesfrom = []
         @responses = 0
         @starttime = Time.now.to_f
@@ -23,7 +24,6 @@ module MCollective
         @discovered_nodes = []
         @okcount = 0
         @failcount = 0
-        @noresponsefrom = []
         @requestid = nil
         @aggregate_summary = []
         @aggregate_failures = []
@@ -32,6 +32,7 @@ module MCollective
       # returns a hash of our stats
       def to_hash
         {:noresponsefrom    => @noresponsefrom,
+         :unexpectedresponsefrom => @unexpectedresponsefrom,
          :starttime         => @starttime,
          :discoverytime     => @discoverytime,
          :blocktime         => @blocktime,
@@ -70,6 +71,7 @@ module MCollective
       # Re-initializes the object with stats from the basic client
       def client_stats=(stats)
         @noresponsefrom = stats[:noresponsefrom]
+        @unexpectedresponsefrom = stats[:unexpectedresponsefrom]
         @responses = stats[:responses]
         @starttime = stats[:starttime]
         @blocktime = stats[:blocktime]
@@ -115,13 +117,19 @@ module MCollective
       def finish_request
         @totaltime = @blocktime + @discoverytime
 
-        # figures out who we had no responses from
+        # figure out who responded unexpectedly
+        rhosts = @responsesfrom.clone
+        @discovered_nodes.each {|d| rhosts.delete(d)}
+        @unexpectedresponsefrom = rhosts
+
+        # figure out who we had no responses from
         dhosts = @discovered_nodes.clone
         @responsesfrom.each {|r| dhosts.delete(r)}
         @noresponsefrom = dhosts
       rescue
         @totaltime = 0
         @noresponsefrom = []
+        @unexpectedresponsefrom = []
       end
 
       # Helper to keep track of who we received responses from
@@ -225,8 +233,22 @@ module MCollective
           end
         end
 
-        if no_response_report != ""
-          result_text << "" << no_response_report
+        no_response_r = no_response_report
+        unexpected_response_r = unexpected_response_report
+        if no_response_r || unexpected_response_r
+          result_text << ""
+        end
+
+        if no_response_r != ""
+          result_text << "" << no_response_r
+        end
+
+        if unexpected_response_r != ""
+          result_text << "" << unexpected_response_r
+        end
+
+        if no_response_r || unexpected_response_r
+          result_text << ""
         end
 
         result_text.join("\n")
@@ -237,7 +259,6 @@ module MCollective
         result_text = StringIO.new
 
         if @noresponsefrom.size > 0
-          result_text.puts
           result_text.puts Util.colorize(:red, "No response from:")
           result_text.puts
 
@@ -248,8 +269,26 @@ module MCollective
           @noresponsefrom.sort.in_groups_of(fields_num) do |c|
             result_text.puts format % c
           end
+        end
 
+        result_text.string
+      end
+
+      # Returns a blob of text indicating what nodes responded but weren't discovered
+      def unexpected_response_report
+        result_text = StringIO.new
+
+        if @unexpectedresponsefrom.size > 0
+          result_text.puts Util.colorize(:red, "Unexpected response from:")
           result_text.puts
+
+          field_size = Util.field_size(@unexpectedresponsefrom, 30)
+          fields_num = Util.field_number(field_size)
+          format = "   " + ( " %-#{field_size}s" * fields_num )
+
+          @unexpectedresponsefrom.sort.in_groups_of(fields_num) do |c|
+            result_text.puts format % c
+          end
         end
 
         result_text.string

spec/unit/mcollective/rpc/stats_spec.rb
@@ -11,6 +11,7 @@ module MCollective
                      :blocktime => 0,
                      :failcount => 0,
                      :noresponsefrom => [],
+                     :unexpectedresponsefrom => [],
                      :responses => 0,
                      :totaltime => 0,
                      :discovered => 0,
@@ -113,7 +114,7 @@ module MCollective
       describe "#client_stats=" do
         it "should store stats correctly" do
           data = {}
-          keys = [:noresponsefrom, :responses, :starttime, :blocktime, :totaltime, :discoverytime]
+          keys = [:noresponsefrom, :unexpectedresponsefrom, :responses, :starttime, :blocktime, :totaltime, :discoverytime]
           keys.each {|k| data[k] = k}
 
           @stats.client_stats = data
@@ -125,7 +126,7 @@ module MCollective
 
         it "should not store discovery time if it was already stored" do
           data = {}
-          keys = [:noresponsefrom, :responses, :starttime, :blocktime, :totaltime, :discoverytime]
+          keys = [:noresponsefrom, :unexpectedresponsefrom, :responses, :starttime, :blocktime, :totaltime, :discoverytime]
           keys.each {|k| data[k] = k}
 
           Time.stubs(:now).returns(Time.at(1300031826))
@@ -231,6 +232,29 @@ module MCollective
           @stats.noresponsefrom.should == ["three"]
         end
 
+        it "should calculate unexpected responses correctly" do
+          Time.stubs(:now).returns(Time.at(1300031824))
+          @stats.time_discovery(:start)
+
+          Time.stubs(:now).returns(Time.at(1300031825))
+          @stats.time_discovery(:end)
+
+          Time.stubs(:now).returns(Time.at(1300031826))
+          @stats.time_block_execution(:start)
+
+          Time.stubs(:now).returns(Time.at(1300031827))
+          @stats.time_block_execution(:end)
+
+          @stats.discovered_agents(["one", "two"])
+          @stats.node_responded("three")
+          @stats.node_responded("one")
+          @stats.node_responded("two")
+
+          @stats.finish_request
+
+          @stats.unexpectedresponsefrom.should == ["three"]
+        end
+
         it "should recover from failure correctly" do
           Time.stubs(:now).returns(Time.at(1300031824))
           @stats.time_discovery(:start)
@@ -249,6 +273,7 @@ module MCollective
           @stats.finish_request
 
           @stats.noresponsefrom.should == []
+          @stats.unexpectedresponsefrom.should == []
           @stats.totaltime.should == 0
         end
       end
@@ -282,6 +307,25 @@ module MCollective
           @stats.finish_request
 
           @stats.no_response_report.should match(Regexp.new(/No response from.+bar\s+foo/m))
+        end
+      end
+
+      describe "#unexpected_response_report" do
+        it "should create an empty report if all responding nodes were discovered" do
+          @stats.discovered_agents ["foo"]
+          @stats.node_responded "foo"
+          @stats.finish_request
+
+          @stats.unexpected_response_report.should == ""
+        end
+
+        it "should list all nodes that did not respond" do
+          @stats.discovered_agents []
+          @stats.node_responded "foo"
+          @stats.node_responded "bar"
+          @stats.finish_request
+
+          @stats.unexpected_response_report.should match(Regexp.new(/Unexpected response from.+bar\s+foo/m))
         end
       end
 

commit d76c1686014096ec35579feabec8f929f6eb8eee

(MCO-785) Fix regression in --batch option
It looks like changes introduced in 928ccac63ae2820bc47679264c12e4adf3ba08c6
broke --batch option, because one of stats hash elements is not
initialized as Array and concat function is called on that object which
leads to crash.

This commit will make sure, that unexpectedresponsefrom key is
initialized as Array.

lib/mcollective/client.rb
@@ -284,6 +284,7 @@ module MCollective
       stat[:blocktime] = stat[:totaltime] - stat[:discoverytime]
       stat[:responses] = hosts_responded
       stat[:noresponsefrom] = []
+      stat[:unexpectedresponsefrom] = []
       stat[:requestid] = requestid
 
       @stats = stat

spec/unit/mcollective/client_spec.rb
@@ -422,7 +422,8 @@ module MCollective
           :totaltime     => 10.0,
           :responses     => 5,
           :requestid     => "erfs123",
-          :noresponsefrom  => [] }
+          :noresponsefrom  => [],
+          :unexpectedresponsefrom => [] }
       end
 
       it "should update stats and return the stats hash" do